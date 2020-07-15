e82691123e2ca5d8fecbdcdb36daae8917f816a2
Soumyaprakash <som01123579@gmail.com>
2020-07-15 23:23:18 +0530

    Added the cart and stripe intgration

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var mongoose=require("mongoose");
 var passport=require("passport");
 var flash=require("connect-flash");
 var validator=require("express-validator");
+var MongoStore=require("connect-mongo")(session);
+
+
+
 var indexRouter = require('./routes/index');
 var UserRouter=require('./routes/user')
 
@@ -32,7 +36,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret:'mysupersecret',resave:false,saveUninitialized:false}));
+app.use(session(
+  {secret:'mysupersecret',
+  resave:false,
+  saveUninitialized:false,
+  store:new MongoStore({mongooseConnection:mongoose.connection}),
+  cookie:{maxAge:180*60*1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
   res.locals.login=req.isAuthenticated();
+  res.locals.session=req.session;
   next();
 })
 
